1deeebe37cdf4fc3d4f79afcaa79580deff4a08f
Wuyun425 <yunwu0425@gmail.com>
2025-04-20 21:12:59 +0800

    Update game.js

--- game.js
+++ game.js
@@ -26,25 +26,25 @@ var game = new Phaser.Game(config);
 function preload() {
     this.load.image('player', 'https://via.placeholder.com/50');
     this.load.image('dog', 'https://via.placeholder.com/50');
-    this.load.image('ground', 'https://via.placeholder.com/800x50'); // 地板
+    this.load.image('ground', 'https://via.placeholder.com/800x50'); 
 }
 
 function create() {
     // 建立靜態平台群組
     platforms = this.physics.add.staticGroup();
-    platforms.create(400, 580, 'ground'); // 地板位置 Y=580
+    platforms.create(400, 580, 'ground'); 
 
     // 玩家
     player = this.physics.add.sprite(100, 450, 'player');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
-    this.physics.add.collider(player, platforms); // 加入碰撞
+    this.physics.add.collider(player, platforms); 
 
     // 狗怪
     dog = this.physics.add.sprite(600, 450, 'dog');
     dog.setBounce(0.2);
     dog.setCollideWorldBounds(true);
-    this.physics.add.collider(dog, platforms); // 狗也跟平台碰撞
+    this.physics.add.collider(dog, platforms); 
 
     cursors = this.input.keyboard.createCursorKeys();
 }
